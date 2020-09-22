476bbd49976ecc9b16438007f92eb39854c87192
yi_Xu <xjy123456120@163.com>
2020-09-23 07:31:00 +0800

    fix input

--- index.js
+++ index.js
@@ -5,15 +5,24 @@ const github = require('@actions/github')
 
 // most @actions toolkit packages have async methods
 async function run() {
-  try {
-    const views_per = core.getInput('views_per', {require: true});
+    const views_per = core.getInput('views_per', {require: false});
+    const clones_per = core.getInput('clones_per', {require: false});
     const octokit = new github.getOctokit(process.env.GITHUB_TOKEN);
-    const { owner, repo } = github.context.repo
-    const views = octokit.repo.getViews({owner:owner,repo:repo,per:views_per})
-    console.log(views)
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+    const { owner, repo } = github.context.repo;
+    try {
+        var views = await octokit.repo.getViews({owner:owner,repo:repo,per:views_per});
+        console.log(views);
+    } catch (error) {
+        console.log(error);
+        core.setFailed(error.message);
+    }
+    try {
+        var clones = await octokit.repo.getViews({owner:owner,repo:repo,per:clones_per});
+        console.log(clones);
+    } catch (error) {
+        console.log(error);
+        core.setFailed(error.message);
+    }
 }
 
 run();

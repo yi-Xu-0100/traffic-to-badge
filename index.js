27fd7f91ec97cab700afd7a020dfd17e2d3eab3a
yi_Xu <xjy123456120@163.com>
2020-09-22 21:33:19 +0800

    test

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const github = require('@actions/github')
+
 
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
-
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
+    const views_per = core.getInput('views', {require: true});
+    const octokit = new github.getOctokit(process.env.GITHUB_TOKEN);
+    const { owner, repo } = github.context.repo
+    const views = octokit.repo.getViews({owner:owner,repo:repo,per:views_per})
+    console.log(views)
   } catch (error) {
     core.setFailed(error.message);
   }

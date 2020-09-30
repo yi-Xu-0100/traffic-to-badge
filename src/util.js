273e5892e993663e1bc2fde8fad0ed04cad04f66
yi_Xu <xjy123456120@163.com>
2020-09-30 17:52:48 +0800

    📝 add debug

--- src/util.js
+++ src/util.js
@@ -105,9 +105,14 @@ let combineTrafficData = async function (traffic_data, traffic_data_path) {
     function combineTypesData(data, data_path, data_type) {
         try {
             var origin_data = JSON.parse(fs.readFileSync(data_path, 'utf8'))[data_type];
+            console.log("origin_data: " + origin_data);
+            console.log("data: " + data[data_type]);
+            console.log("pluck: " + _.pluck(origin_data, 'timestamp'));
             var today_data = _.filter(data[data_type],
                 (a) => !_.contains(_.pluck(origin_data, 'timestamp'), a.timestamp));
+            console.log("today_data: " + today_data);
             today_data = _.union(origin_data, today_data);
+            console.log("today_data_union: " + today_data);
             var count = today_data.map(el => parseInt(el.count)).reduce((a, b) => a + b, 0);
             var uniques = today_data.map(el => parseInt(el.uniques)).reduce((a, b) => a + b, 0);
             data = _.extend({ 'count': count }, { 'uniques': uniques }, { [`${data_type}`]: today_data });
